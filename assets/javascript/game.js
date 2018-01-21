72021dfbc2b208ba6a4da2ba1e1aff6ca6da0d0c
adkahane <adam.kahane.nj@gmail.com>
2018-01-20 17:11:48 -0800

    working game logic and click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,23 @@
 var crystValues = [];
-var crystImg = [];
+var crystImg = ["assets/images/bitcoin.png", 
+				"assets/images/dogecoin.png", 
+				"assets/images/ethereum.png", 
+				"assets/images/litecoin.png"];
 var wins = 0;
 var losses = 0;
-var userTotal;
+var userTotal = 0;
 
 // Functions
 
-function randomNum() {
+// Creates a random number between min and max
+function randomNum(min, max) {
   // console.log(wordList);
-  return ((Math.floor(Math.random()) + 19) * 120);
+  return (Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
-// Checks if the user shas guessed the letter before
+// Creates a random number between 1 and 12
+
+// Checks if the random number has been used for any other crystals
 function compareUsed(ranNum) {
   for(i = 0; i < crystValues.length; i++) {
     if (ranNum === crystValues[i]) {
@@ -20,33 +26,74 @@ function compareUsed(ranNum) {
   }
 }
 
+// Makes an array of 4 crystal values with values between 1 and 12
 function makeCrystalValue() {
-	var ranNum = randomNum();
+	var ranNum = randomNum(1, 12);
 	if (compareUsed(ranNum) === true) {
 		makeCrystalvalue();
 	}	
-	else () {
+	else {
 		crystValues.push(ranNum);
+	}
+	console.log(crystValues);
+}
+
+// Creates crystal img's assigns a data value and an image source
+function makeCrystals() {
+	for (var i = 0; i < 4; i++) {
+    	// For each iteration, we will create an imageCrystal
+    	var imageCrystal = $("<img>");
+
+    	// First each crystal will be given the class ".crystal-image".
+    	// This will allow the CSS to take effect.
+    	imageCrystal.addClass("crystal-image");
+
+    	// Each imageCrystal will be given a src link to the crystal image
+    	imageCrystal.attr("src", crystImg[i]);
+
+    	// Each imageCrystal will be given a data attribute called data-crystalValue.
+    	// This data attribute will be set equal to the array value.
+    	imageCrystal.attr("data-crystalvalue", crystValues[i]);
+
+    	// Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    	$("#crystals").append(imageCrystal);
 	}
 }
 
-function makeCrystals() {
-	for (var i = 0; i < 4; i++) {
+// This is the game
+function theGame() {
+	for(i = 0; i < 4; i++) {
+		makeCrystalValue();
+	}
+	makeCrystals();
+	var goal = randomNum(19, 120);
+	console.log(goal);
+	$(".crystal-image").on("click", function() {
 
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
+    	// Determining the crystal's value requires us to extract the value from the data attribute.
+    	// Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+    	// Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    	// Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+    
+    	var crystalValue = ($(this).attr("data-crystalvalue"));
+    	crystalValue = parseInt(crystalValue);
 
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
+    	// We then add the crystalValue to the user's "counter" which is a global variable.
+    	// Every click, from every crystal adds to the global counter.
+    	userTotal += crystalValue;
 
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", crystalImg[i]);
+    	// All of the same game win-lose logic applies. So the rest remains unchanged.
+    	alert("New score: " + userTotal);
+    	console.log(goal);
+    	if (userTotal === goal) {
+      		alert("You win!");
+    	}
 
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", crystValues[i]);
+    	else if (userTotal >= goal) {
+      		alert("You lose!!");
+    	}
 
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+	});
 }
+
+theGame();
